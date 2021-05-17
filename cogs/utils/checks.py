bfe1643c868fe90f95cef8279d80ce5e7662ee74
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2021-05-17 19:16:05 +0530

    Update checks.py
    updated checks .py

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -1,4 +1,27 @@
 import discord
 from discord.ext import command
 
+# These do not take channel overrides into account
+
+def is_mod():
+    async def pred(ctx):
+        return await check_guild_permissions(ctx, {'manage_guild': True})
+    return commands.check(pred)
+
+def is_admin():
+    async def pred(ctx):
+        return await check_guild_permissions(ctx, {'administrator': True})
+    return commands.check(pred)
+
+def mod_or_permissions(**perms):
+    perms['manage_guild'] = True
+    async def predicate(ctx):
+        return await check_guild_permissions(ctx, perms, check=any)
+    return commands.check(predicate)
+
+def admin_or_permissions(**perms):
+    perms['administrator'] = True
+    async def predicate(ctx):
+        return await check_guild_permissions(ctx, perms, check=any)
+    return commands.check(predicate)
 
