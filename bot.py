da09e683cd59ad745a59c4f969cacdc76bb3c849
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2021-05-17 21:03:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,4 +2,30 @@ import discord,sys,os,io,time,datetime,json,asyncio,aiohttps
 import config as C
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix= prefix)
+def get_prefix(client, message): ##first we define get_prefix
+    with open('prefixes.json', 'r') as f: ##we open and read the prefixes.json, assuming it's in the same file
+        prefixes = json.load(f) #load the json as prefixes
+    return prefixes[str(message.guild.id)] 
+
+bot = commands.Bot(
+    command_prefix= (get_prefix),
+    )
+@bot.event
+async def on_guild_join(guild): #when the bot joins the guild
+    with open('prefixes.json', 'r') as f: #read the prefix.json file
+        prefixes = json.load(f) #load the json file
+
+    prefixes[str(guild.id)] = C.default_prefix #default prefix
+
+    with open('prefixes.json', 'w') as f: #write in the prefix.json "message.guild.id": "bl!"
+        json.dump(prefixes, f, indent=4) #the indent is to make everything look a bit neater
+
+@bot.event
+async def on_guild_remove(guild): #when the bot is removed from the guild
+    with open('prefixes.json', 'r') as f: #read the file
+        prefixes = json.load(f)
+
+    prefixes.pop(str(guild.id)) #find the guild.id that bot was removed from
+
+    with open('prefixes.json', 'w') as f: #deletes the guild.id as well as its prefix
+        json.dump(prefixes, f, indent=4)
