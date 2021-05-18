5fac5c5648f02fb2e047f00214e72ccf5a90fa6b
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2021-05-18 08:13:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord,sys,os,io,time,datetime,json,asyncio,aiohttps
 import config as C
+from .cogs.utils import checks 
 from discord.ext import commands
 
 def get_prefix(client, message): ##first we define get_prefix
@@ -29,3 +30,11 @@ async def on_guild_remove(guild): #when the bot is removed from the guild
 
     with open('prefixes.json', 'w') as f: #deletes the guild.id as well as its prefix
         json.dump(prefixes, f, indent=4)
+
+@bot.command()
+@checks.is_admin()
+async def setprefix(ctx,prefix:str):
+    with open('prefixs.json','r+') as f:
+        n = json.load(f)
+    n[str(ctx.guild.id)] = prefix
+    await ctx.reply(f"changed prefix in this guild to `{prefix}`")
