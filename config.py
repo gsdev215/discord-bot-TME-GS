5fa8459422a7b04efbe2956a2a4a74ea60362122
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2021-05-19 16:12:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,17 @@
+import json
 token = ""
-default_prefix = ""
+default_prefix = "."
 devs_id = ""
 support_server = ""
+cog = ["cogs.leveling"]
+
+def json_open(f:str=None):
+	with open(f,"r") as f:
+		n = json.load(f)
+	return n
+	
+def json_dump(f:str=None,n:str=None):
+	#f = f"{f}"
+	#n = f"{d}"
+	with open(f,"w") as f:
+		json.dump(n,f)
