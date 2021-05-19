a62e6cce21da081c2c80dc6452b9a760cf2f8d1f
gsdev215 <74264993+gsdev215@users.noreply.github.com>
2021-05-19 16:15:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,50 @@
-import discord,sys,os,io,time,datetime,json,asyncio,aiohttps
+import discord,sys,os,io,time,datetime,json,asyncio
 import config as C
-from .cogs.utils import checks 
+from cogs.utils import checks
 from discord.ext import commands
 
-def get_prefix(client, message): ##first we define get_prefix
+def get_prefix(bot, message): ##first we define get_prefix
     with open('prefixes.json', 'r') as f: ##we open and read the prefixes.json, assuming it's in the same file
         prefixes = json.load(f) #load the json as prefixes
-    return prefixes[str(message.guild.id)] 
+    try:
+    	return prefixes[str(message.guild.id)]
+    except:
+    	return C.default_prefix
+
+intents = discord.Intents().default()
+intents.messages = True
+intents.reactions = True
+intents.presences = True
+intents.members = True
+intents.guilds = True
+intents.emojis = True
+intents.bans = True
+intents.guild_typing = False
+intents.typing = False
+intents.dm_messages = False
+intents.dm_reactions = False
+intents.dm_typing = False
+intents.guild_messages = True
+intents.guild_reactions = True
+intents.integrations = True
+intents.invites = True
+intents.voice_states = False
+intents.webhooks = False
 
 bot = commands.Bot(
     command_prefix= (get_prefix),
-    )
+    intents=intents)
 @bot.event
-async def on_guild_join(guild): #when the bot joins the guild
-    with open('prefixes.json', 'r') as f: #read the prefix.json file
-        prefixes = json.load(f) #load the json file
-
-    prefixes[str(guild.id)] = C.default_prefix #default prefix
-
-    with open('prefixes.json', 'w') as f: #write in the prefix.json "message.guild.id": "bl!"
-        json.dump(prefixes, f, indent=4) #the indent is to make everything look a bit neater
-
+async def on_ready():
+	print("online")
+	
 @bot.event
 async def on_guild_remove(guild): #when the bot is removed from the guild
     with open('prefixes.json', 'r') as f: #read the file
         prefixes = json.load(f)
-
-    prefixes.pop(str(guild.id)) #find the guild.id that bot was removed from
+    
+    g = f"{guild.id}"
+    prefixes.pop(g) #find the guild.id that bot was removed from
 
     with open('prefixes.json', 'w') as f: #deletes the guild.id as well as its prefix
         json.dump(prefixes, f, indent=4)
@@ -34,9 +52,29 @@ async def on_guild_remove(guild): #when the bot is removed from the guild
 @bot.command()
 @checks.is_admin()
 async def setprefix(ctx,prefix:str):
-    with open('prefixs.json','r') as f:
+    with open('prefixes.json','r') as f:
         n = json.load(f)
-    n[str(ctx.guild.id)] = prefix
-    with open("prefixs.json","w") as f:
+    s = f"{ctx.guild.id}"
+    n[s] = prefix
+    with open("prefixes.json","w") as f:
         json.dump(n,f)
     await ctx.reply(f"changed prefix in this guild to `{prefix}`")
+     
+@bot.command()
+async def run(ctx,*,c):
+	e = f"{eval(c)}"
+	await ctx.send(e)
+	
+if __name__ == "__main__":
+	for extension in C.cog:
+		try:
+			bot.load_extension(extension)
+			extension = extension.replace("cogs.", "")
+			print(f"Loaded extension '{extension}'")
+		except Exception as e:
+			exception = f"{type(e).__name__}: {e}"
+			extension = extension.replace("cogs.", "")
+			print(f"Failed to load extension {extension}\n{exception}")
+			      
+
+bot.run(C.token)
